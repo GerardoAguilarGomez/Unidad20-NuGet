e769b2534d4fea754d94942c1660c504e20f1110
GerardoAguilarGomez <gerardo.aguilar@grupocastilla.es>
2021-01-27 11:02:02 +0100

    Pruebas NuGet

---
+++ PruebasNuget/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PruebasNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
